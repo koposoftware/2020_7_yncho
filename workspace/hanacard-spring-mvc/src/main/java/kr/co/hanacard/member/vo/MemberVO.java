776d36a32bef9b6a506891de2a6a5ced7df2b237
yeongnam-jo <datetime1929@gmail.com>
2020-09-02 22:19:37 +0900

    MemberVO 멤버변수 14개 추가(카드사)

--- workspace/hanacard-spring-mvc/src/main/java/kr/co/hanacard/member/vo/MemberVO.java
+++ workspace/hanacard-spring-mvc/src/main/java/kr/co/hanacard/member/vo/MemberVO.java
@@ -6,6 +6,23 @@ public class MemberVO {
 	private String password;
 	private String type;
 	private String resiNum;
+	
+	private String cha;
+	private String csh;
+	private String css;
+	private String chd;
+	private String ckm;
+	private String clt;
+	private String cwr;
+	private String cbc;
+	private String cct;
+	private String cnh;
+	private String cshb;
+	private String ckjb;
+	private String cjbb;
+	private String cjjb;
+	
+	
 	public String getId() {
 		return id;
 	}
@@ -30,9 +47,97 @@ public class MemberVO {
 	public void setResiNum(String resiNum) {
 		this.resiNum = resiNum;
 	}
+	public String getCha() {
+		return cha;
+	}
+	public void setCha(String cha) {
+		this.cha = cha;
+	}
+	public String getCsh() {
+		return csh;
+	}
+	public void setCsh(String csh) {
+		this.csh = csh;
+	}
+	public String getCss() {
+		return css;
+	}
+	public void setCss(String css) {
+		this.css = css;
+	}
+	public String getChd() {
+		return chd;
+	}
+	public void setChd(String chd) {
+		this.chd = chd;
+	}
+	public String getCkm() {
+		return ckm;
+	}
+	public void setCkm(String ckm) {
+		this.ckm = ckm;
+	}
+	public String getClt() {
+		return clt;
+	}
+	public void setClt(String clt) {
+		this.clt = clt;
+	}
+	public String getCwr() {
+		return cwr;
+	}
+	public void setCwr(String cwr) {
+		this.cwr = cwr;
+	}
+	public String getCbc() {
+		return cbc;
+	}
+	public void setCbc(String cbc) {
+		this.cbc = cbc;
+	}
+	public String getCct() {
+		return cct;
+	}
+	public void setCct(String cct) {
+		this.cct = cct;
+	}
+	public String getCnh() {
+		return cnh;
+	}
+	public void setCnh(String cnh) {
+		this.cnh = cnh;
+	}
+	public String getCshb() {
+		return cshb;
+	}
+	public void setCshb(String cshb) {
+		this.cshb = cshb;
+	}
+	public String getCkjb() {
+		return ckjb;
+	}
+	public void setCkjb(String ckjb) {
+		this.ckjb = ckjb;
+	}
+	public String getCjbb() {
+		return cjbb;
+	}
+	public void setCjbb(String cjbb) {
+		this.cjbb = cjbb;
+	}
+	public String getCjjb() {
+		return cjjb;
+	}
+	public void setCjjb(String cjjb) {
+		this.cjjb = cjjb;
+	}
+	
 	@Override
 	public String toString() {
-		return "MemberVO [id=" + id + ", password=" + password + ", type=" + type + ", resiNum=" + resiNum + "]";
+		return "MemberVO [id=" + id + ", password=" + password + ", type=" + type + ", resiNum=" + resiNum + ", cha="
+				+ cha + ", csh=" + csh + ", css=" + css + ", chd=" + chd + ", ckm=" + ckm + ", clt=" + clt + ", cwr="
+				+ cwr + ", cbc=" + cbc + ", cct=" + cct + ", cnh=" + cnh + ", cshb=" + cshb + ", ckjb=" + ckjb
+				+ ", cjbb=" + cjbb + ", cjjb=" + cjjb + "]";
 	}
 	
 	
