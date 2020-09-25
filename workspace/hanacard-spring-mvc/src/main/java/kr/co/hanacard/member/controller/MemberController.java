64601e52adf1b06507296834e0fec840b8cf5305
yeongnam-jo <datetime1929@gmail.com>
2020-09-25 11:29:09 +0900

    Interceptor 생성 및 Prehandler 정의

--- workspace/hanacard-spring-mvc/src/main/java/kr/co/hanacard/member/controller/MemberController.java
+++ workspace/hanacard-spring-mvc/src/main/java/kr/co/hanacard/member/controller/MemberController.java
@@ -76,7 +76,23 @@ public class MemberController {
 			mav.setViewName("redirect:/login");
 		} else {
 			// 로그인 성공
-			mav.setViewName("redirect:/"); // ModelAndView 생성자에 넣으면 자동으로 setViewName이 되는거였겠군?
+			
+			
+			String dest = (String) session.getAttribute("dest");
+			System.out.println("dest ========> " + dest);
+			System.out.println("dest ========> " + dest);
+			System.out.println("dest ========> " + dest);
+			System.out.println("dest ========> " + dest);
+			
+			if(dest==null) {
+				
+				mav.setViewName("redirect:/"); // ModelAndView 생성자에 넣으면 자동으로 setViewName이 되는거였겠군?
+				
+			} else {
+				mav.setViewName("redirect:" + dest); // dest 값은 이런 식이다. /board/2   즉 => http://localhost:9999/Mission-Spring/board/2
+				session.removeAttribute("dest"); //이제 이 정보가 필요 없기 때문에 세션에 등록된 dest 객체를 지운다.
+			}
+			
 			mav.addObject("loginVO", loginVO);
 			// 이렇게 하면 사용자 정보가 세션에 저장되지 않는다. 왜? addObject() 하면 request 영역에 저장되기 때문이다.
 			// 그렇게 하려면 어떻게 해야하는가 : 클래스 위에 @SessionAttributes 어노테이션을 붙인다.
